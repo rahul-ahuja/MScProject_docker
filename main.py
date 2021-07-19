d57843668b777e3010b2c0e049fff099efcd455f
rahul-ahuja <rahul.ahuja@live.com>
2021-07-20 00:11:59 +0100

    change the env variables

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-postgres_pwd = ${{ secrets.POSTGRES_PASSWORD }}
-postgres_user = ${{ secrets.POSTGRES_USER }}
+postgres_pwd = ${{ env.POSTGRES_PASSWORD }}
+postgres_user = ${{ env.POSTGRES_USER }}
 #making connection to the postgres database
 try:
     conn = psycopg2.connect(f'host=localhost user={postgres_user} password={postgres_pwd}')
