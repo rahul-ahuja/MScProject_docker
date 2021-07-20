caf293e1f0df98f6b3d2ef152b209598efb6548b
rahul-ahuja <rahul.ahuja@live.com>
2021-07-20 01:38:33 +0100

    again revert

--- models.py
+++ models.py
@@ -53,14 +53,14 @@ making user the table with same does not exists
 
 cur.execute('''DROP TABLE IF EXISTS proposals''')
 cur.execute('''DROP TABLE IF EXISTS requests''')
-cur.execute('''DROP TABLE IF EXISTS cs_users''')
+cur.execute('''DROP TABLE IF EXISTS users''')
 
 #creating users tables in the schema
-cur.execute('''CREATE TABLE IF NOT EXISTS cs_users (username TEXT NOT NULL PRIMARY KEY,
+cur.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT NOT NULL PRIMARY KEY,
     hash TEXT NOT NULL)''')
 
 #creating request tables in the schema
-cur.execute('''CREATE TABLE IF NOT EXISTS cs_requests (id SERIAL PRIMARY KEY,
+cur.execute('''CREATE TABLE IF NOT EXISTS requests (id SERIAL PRIMARY KEY,
         username TEXT NOT NULL,
         meal_type TEXT NOT NULL,
         location TEXT NOT NULL,
@@ -68,23 +68,23 @@ cur.execute('''CREATE TABLE IF NOT EXISTS cs_requests (id SERIAL PRIMARY KEY,
 
 
 #creating proposals tables in the schema
-cur.execute('''CREATE TABLE IF NOT EXISTS cs_proposals (id SERIAL PRIMARY KEY,
+cur.execute('''CREATE TABLE IF NOT EXISTS proposals (id SERIAL PRIMARY KEY,
         request_id INTEGER NOT NULL,
         user_to TEXT NOT NULL,
         user_from TEXT NOT NULL)''')
 
 
 
-cur.execute('''ALTER TABLE cs_requests ADD CONSTRAINT fk_requests 
-        FOREIGN KEY (username) REFERENCES cs_users(username)''')
+cur.execute('''ALTER TABLE requests ADD CONSTRAINT fk_requests 
+        FOREIGN KEY (username) REFERENCES users(username)''')
 
-cur.execute('''ALTER TABLE cs_proposals ADD CONSTRAINT fk_proposals 
-        FOREIGN KEY (request_id) REFERENCES cs_requests(id)''')
+cur.execute('''ALTER TABLE proposals ADD CONSTRAINT fk_proposals 
+        FOREIGN KEY (request_id) REFERENCES requests(id)''')
 
-cur.execute('''ALTER TABLE cs_proposals ADD CONSTRAINT fk_proposals_userto 
-        FOREIGN KEY (user_to) REFERENCES cs_users(username)''')
+cur.execute('''ALTER TABLE proposals ADD CONSTRAINT fk_proposals_userto 
+        FOREIGN KEY (user_to) REFERENCES users(username)''')
 
-cur.execute('''ALTER TABLE cs_proposals ADD CONSTRAINT fk_proposals_userfrom 
-        FOREIGN KEY (user_from) REFERENCES cs_users(username)''')
+cur.execute('''ALTER TABLE proposals ADD CONSTRAINT fk_proposals_userfrom 
+        FOREIGN KEY (user_from) REFERENCES users(username)''')
 
 conn.close() #closing the database connection
